ddcec3d5b341153263cc72c6fdfb928859efff1a
Derek Hoagland <thegrickit@gmail.com>
2012-02-25 08:31:28 -0500

    reduce code duplication

--- source/ImageManager.js
+++ source/ImageManager.js
@@ -13,18 +13,21 @@
       }
       else if(this.graphics[imageURL].loaded = true) { // Exists and is loaded
 	console.log('Graphic "'+imageURL+'" already loaded.');
-	images.unloaded--;
-	if(callback_object != undefined) { callback_object[callback_function](callback_arguments); }
+	this.loaded(callback_object,callback_function,callback_arguments);
       }
       else {
 	this.graphics[imageURL].addEventListener('load',function(thisObj) { // Exists but has not loaded yet
-	  thisObj.graphics[imageURL].loaded = true;
-	  thisObj.unloaded--;
+	  this.loaded = true;
 	  console.log('Graphic "'+imageURL+'" finished loading.');
-	  if(callback_object != undefined) { callback_object[callback_function](callback_arguments); }
+	  thisObj.loaded(callback_object,callback_function,callback_arguments);
 	},this);
 	this.graphics[imageURL].src = 'images/'+imageURL+'.png';
       }
+    }
+
+    this.loaded = function loaded(callback_object,callback_function,callback_arguments) {
+      this.unloaded--;
+      if(callback_object != undefined) { callback_object[callback_function](callback_arguments); }
     }
 
     this.get = function(imageURL) {
