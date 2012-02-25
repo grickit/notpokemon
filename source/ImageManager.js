1935d621f835debeaff7a082b9a24c4bfe38870e
Derek Hoagland <thegrickit@gmail.com>
2012-02-25 08:15:14 -0500

    Create new ImageManager class so that I can potentially have multiple sets of images.

---
+++ source/ImageManager.js
@@ -0,0 +1,30 @@
+// ----- CLASS: ImageManager {
+  function ImageManager {
+    this.unloaded = 0;
+    this.graphics = new Array();
+
+    this.load = function(imageURL,callback_object,callback_function,callback_arguments) {
+      if(imageURL == undefined) { throw "Tried to load an image with an undefined imageURL"; }
+      this.unloaded++;
+
+      if(this.graphics[imageURL] == undefined) { // Doesn't exist
+	this.graphics[imageURL] = new Image();
+	this.graphics[imageURL].loaded = false;
+      }
+      else if(this.graphics[imageURL].loaded = true) { // Exists and is loaded
+	console.log('Graphic "'+imageURL+'" already loaded.');
+	images.unloaded--;
+	if(callback_object != undefined) { callback_object[callback_function](callback_arguments); }
+      }
+      else {
+	this.graphics[imageURL].addEventListener('load',function(thisObj) { // Exists but has not loaded yet
+	  thisObj.graphics[imageURL].loaded = true;
+	  thisObj.unloaded--;
+	  console.log('Graphic "'+imageURL+'" finished loading.');
+	  if(callback_object != undefined) { callback_object[callback_function](callback_arguments); }
+	},this);
+	this.graphics[imageURL].src = 'images/'+imageURL+'.png';
+      }
+    }
+  }
+// ----- }
