417df1bf82147d8855771d3d350fd711f9e34d4a
Derek Hoagland <thegrickit@gmail.com>
2012-02-27 18:19:27 -0500

    Change facing even if you can't step in a certain direction.

--- source/EntityLiving.js
+++ source/EntityLiving.js
@@ -17,8 +17,8 @@
       var newx = this.x + game.directionChanges[direction].x;
       var newy = this.y + game.directionChanges[direction].y;
       this.facing = game.directionNumbers[direction];
+      this.current_image = this.sprites[this.facing];
       if(game.getTile(this.x, this.y).clipfrom[game.directionNumbers[direction]] && game.getTile(newx,newy).clipto[game.directionNumbers[direction]]) {
-	this.current_image = this.sprites[game.directionNumbers[direction]];
 	this.current_image.playOnce();
 	this.z = 120;
 	this.current_image.on_finished.subscribe(undefined,function(thisObj){ thisObj.setPosition(newx,newy); thisObj.current_image.stop(); thisObj.z = 100; },this);
