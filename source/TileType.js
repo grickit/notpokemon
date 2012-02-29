b9443fbaf6d8d677ef78ac8af7ef3c6f8ed671cc
Derek Hoagland <thegrickit@gmail.com>
2012-02-29 09:47:44 -0500

    new TileType class

---
+++ source/TileType.js
@@ -0,0 +1,13 @@
+//# CLEAN
+
+// ----- CLASS: TileType {
+  function TileType(args) {
+    if (args.code == undefined) { throw "new tileType missing required code attribute"; }
+    else { this.letter = args.letter; }
+
+    this.group = (args.group == undefined)? this.letter : args.group;
+    this.clipto = (args.clipto == undefined)? [false,false,false,false]: args.clipto;
+    this.clipfrom = (args.clipfrom == undefined)? [true,true,true,true]: args.clipfrom;
+    this.overlays = args.overlays;
+  }
+// ----- }
