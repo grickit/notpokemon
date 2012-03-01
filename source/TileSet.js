85e75f277be735806fd73cd9b30a6f7987df8b3d
Derek Hoagland <thegrickit@gmail.com>
2012-03-01 12:43:52 -0500

    Some error handling for TileSet.

--- source/TileSet.js
+++ source/TileSet.js
@@ -11,7 +11,9 @@
       return this.uid++;
     }
 
-    this.add = function() {
+    this.add = function(args) {
+      if(args == undefined) { throw "Tried to create a new TileType without any arguments."; }
+      if(args.code == undefined) { args.code = this.getUniqueTileID(); }
       if(this.tiles[args.code] != undefined) { throw "New TileType trying to use code '"+code+"', which is already used in this TileSet"; }
       this.tiles[args.code] = new TileType(args);
     }
