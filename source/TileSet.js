97cb2ab4b1003c6339dbc68f1865d34b7eb5c653
Derek Hoagland <thegrickit@gmail.com>
2012-03-02 12:01:14 -0500

    For reasons that should have been fairly obvious, we can't generate tile IDs.

--- source/TileSet.js
+++ source/TileSet.js
@@ -4,18 +4,13 @@
   function TileSet() {
     // Properties
     this.tiles = new Object();
-    this.uid = 0;
 
     // Methods
-    this.getUniqueTileID = function() {
-      return this.uid++;
-    }
-
     this.add = function(args) {
       if(args == undefined) { throw "Tried to create a new TileType without any arguments."; }
-      if(args.code == undefined) { args.code = this.getUniqueTileID(); }
+      if(args.code == undefined) { throw "new tileType missing required code attribute"; }
       if(this.tiles[args.code] != undefined) { throw "New TileType trying to use code '"+code+"', which is already used in this TileSet"; }
-      this.tiles[args.code] = new TileType(args);
+      this.tiles[args.code] = new TileType(args,this);
     }
 
     // Initialize
