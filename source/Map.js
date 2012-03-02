4fda7b0d4bd59986356fdb130b9aeb17e3c3a094
Derek Hoagland <thegrickit@gmail.com>
2012-03-02 11:40:56 -0500

    Add a safe way to resize maps.

--- source/Map.js
+++ source/Map.js
@@ -9,30 +9,28 @@
     this.entities = new Array();
 
     // Methods
+    this.setDimensions = function(width,height) {
+      newtiles = new Array();
+      newentities = new Array();
+      for(x = 0; x <= width; x++) {
+	if(this.tiles[x] == undefined) { newtiles[x] = new Array(); }
+	else { newtiles[x] = this.tiles[x]; }
+	if(this.entities[x] == undefined) { newentities[x] = new Array(); }
+	else { newentities[x] = this.entities[x]; }
+	for(y = 0; y <= height; y++) {
+	  if(this.tiles[x] == undefined || this.tiles[x][y] == undefined) { newtiles[x][y] = 'g'; }
+	  else { newtiles[x][y] = this.tiles[x][y]; }
+	  if(this.entities[x] == undefined || this.entities[x][y] == undefined) { newentities[x][y] = new Object(); }
+	  else { newentities[x][y] = this.entities[x][y]; }
+	}
+      }
+      this.tiles = newtiles;
+      this.entities = newentities;
+      this.width = width;
+      this.height = height;
+    }
 
     // Initialize
-    this.tiles = [
-      ['r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r'],
-      ['r', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'r'],
-      ['r', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'r'],
-      ['r', 'g', 'g', 'g', 'gfr', 'g', 'g', 'g', 'g', 'g', 'g', 'r'],
-      ['r', 'g', 'g', 'g', 'g', 'g', 'g', 'r', 'g', 'g', 'g', 'r'],
-      ['r', 'g', 'g', 'g', 'g', 'g', 'g', 'r', 'r', 'g', 'g', 'r'],
-      ['r', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'r', 'g', 'g', 'r'],
-      ['r', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'r', 'g', 'g', 'r'],
-      ['r', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'r', 'g', 'g', 'r'],
-      ['r', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'r'],
-      ['r', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'r'],
-      ['r', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'r'],
-      ['r', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'r'],
-      ['r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r']
-    ];
-
-    for(x = 0; x < this.tiles.length; x++) {
-      this.entities.push(new Array());
-      for(y = 0; y < this.tiles[x].length; y++) {
-	this.entities[x].push(new Object());
-      }
-    }
+    this.setDimensions(width,height);
   }
 // ----- }
