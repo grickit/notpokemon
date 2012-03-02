b519e9e69f98f4df1b7678fd0e4cabb8b4964200
Derek Hoagland <thegrickit@gmail.com>
2012-03-02 12:03:48 -0500

    TileType inheritance

--- source/TileType.js
+++ source/TileType.js
@@ -1,13 +1,21 @@
 //# CLEAN
 
 // ----- CLASS: TileType {
-  function TileType(args) {
+  function TileType(args,set) {
     if (args.code == undefined) { throw "new tileType missing required code attribute"; }
     else { this.letter = args.letter; }
-    this.sprite = (args.sprite == undefined)? new Sprite({imageURL: 'tiles/redflower', x: 0, y: 0, duration: 700}) : args.sprite;
-    this.group = (args.group == undefined)? this.letter : args.group;
-    this.clipto = (args.clipto == undefined)? [false,false,false,false] : args.clipto;
-    this.clipfrom = (args.clipfrom == undefined)? [true,true,true,true] : args.clipfrom;
+    this.childof = args.childof;
     this.overlays = args.overlays;
+
+    if(this.childof == undefined || set.tiles[this.childof] == undefined) {
+      this.sprite = (args.sprite == undefined)? new Sprite({imageURL: 'tiles/redflower', x: 0, y: 0, duration: 700}) : args.sprite;
+      this.clipto = (args.clipto == undefined)? [false,false,false,false] : args.clipto;
+      this.clipfrom = (args.clipfrom == undefined)? [true,true,true,true] : args.clipfrom;
+    }
+    else {
+      this.sprite = (args.sprite == undefined)? set[this.childof].sprite : args.sprite;
+      this.clipto = (args.clipto == undefined)? set[this.childof].clipto : args.clipto;
+      this.clipfrom = (args.clipfrom == undefined)? set[this.childof].clipto : args.clipfrom;
+    }
   }
 // ----- }
