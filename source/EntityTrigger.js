1c192e3221abc84608cf042d54f9c4b6c793a251
Derek Hoagland <thegrickit@gmail.com>
2012-05-28 08:14:39 -0400

    Improvements to EntityTrigger

--- source/EntityTrigger.js
+++ source/EntityTrigger.js
@@ -1,19 +1,18 @@
-//# CLEAN but has lots of temporary code
-
 // ----- CLASS: EntityTiving {
   function EntityTrigger(args,map_override) {
-    this.prototype = new Entity(args); // Inherit Entity
+    var self = this;
+    this.prototype = new Entity(args,map_override); // Inherit Entity
     Entity.call(this,args,map_override); // Inherit Entity
 
     // ----- Properties
 
     // ----- Methods
-    this.on_render = function() {
-      if(game.mousedown) { this.current_image = this.sprites[0]; }
-      else { this.current_image = this.sprites[1]; }
+    self.on_render = function() {
+      if(game.mousedown) { self.current_image = self.sprites[0]; }
+      else { self.current_image = self.sprites[1]; }
     }
 
     // ----- Initialize
-    this.sprites.push(new Sprite({imageURL: 'alpha'}));
+    self.sprites.push(new Sprite({imageURL: 'alpha'}));
   }
 // ----- }
