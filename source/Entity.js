19fe934eaea8c79b0785cb750ff77a040026cfd0
Derek Hoagland <thegrickit@gmail.com>
2012-07-04 13:33:15 -0400

    Clean up entity.js with the new OOP paradigm

--- source/Entity.js
+++ source/Entity.js
@@ -2,47 +2,49 @@
 
 // ----- CLASS: Entity {
   function Entity(args,map_override) {
+    var self = this;
     // ----- Properties
-    this.name = (args.name == undefined)? uniqueEntityID() : args.name;
-    this.x = (args.x == undefined)? 0 : args.x;
-    this.y = (args.y == undefined)? 0 : args.y;
-    this.z = (args.z == undefined)? 100 : args.z;
-    this.map = (map_override == undefined)? game.currentMap : map_override;
+    self.name = isset(args.name)? args.name : uniqueEntityID();
+    self.x = isset(args.x)? args.x : 0;
+    self.y = isset(args.y)? args.y : 0;
+    self.z = isset(args.z)? args.z : 100;
+    self.map = isset(map_override)? map_override : game.currentMap;
     // TODO: sprites should be an associative array named animations
-    this.sprites = (args.sprites == undefined)? [new Sprite({imageURL: 'characters/sign', yoffset: -1})] : args.sprites;
+    self.sprites = isset(args.sprites)? args.sprites : [new Sprite({imageURL: 'characters/sign', yoffset: -1})];
 
     // ----- Methods
-    this.on_render = (args.on_render == undefined)? function(){} : args.on_render;
-    this.on_tick = (args.on_tick == undefined)? function(){} : args.on_tick;
-    this.on_moved_to = (args.on_moved_to == undefined)? function(){} : args.on_moved_to;
-    this.on_moved_from = (args.on_moved_from == undefined)? function(){} : args.on_moved_from;
+    self.on_render = isset(args.on_render)? args.on_render : function(){};
+    self.on_tick = isset(args.on_tick)? args.on_tick : function(){};
+    self.on_moved_to = isset(args.on_moved_to)? args.on_moved_to : function(){};
+    self.on_moved_from = isset(args.on_moved_from)? args.on_moved_from : function(){};
 
-    this.purge = function() {
-      delete this.map.entities[this.x][this.y][this.name];
-      delete game.entities[this.name];
-      delete game.on_tick.subscribers[this.game_tick_subscription_number]; // TODO: callback() needs an unsubscribe method
+    self.purge = function() {
+      delete self.map.entities[self.x][self.y][self.name];
+      delete game.entities[self.name];
+      game.on_tick.unsubscribe[self.game_tick_subscription_number];
+      delete self;
       delete this;
     }
 
-    this.getImage = function() {
-      if(this.current_image instanceof Animation) { return this.current_image.current; }
-      else if(this.current_image instanceof Sprite) { return this.current_image; }
-      else { throw "Current image of entity \""+this.name+"\" is of unknown type."; }
+    self.getImage = function() {
+      if(self.current_image instanceof Animation) { return self.current_image.current; }
+      else if(self.current_image instanceof Sprite) { return self.current_image; }
+      else { throw "Current image of entity \""+self.name+"\" is of unknown type."; }
     }
 
-    this.setPosition = function(x,y) {
-      delete this.map.entities[this.x][this.y][this.name];
-      for(name in this.map.entities[this.x][this.y]) { this.map.entities[this.x][this.y][name].on_moved_from(this); }
-      this.x = x;
-      this.y = y;
-      for(name in this.map.entities[this.x][this.y]) { this.map.entities[this.x][this.y][name].on_moved_to(this); }
-      this.map.entities[this.x][this.y][this.name] = this;
+    self.setPosition = function(x,y) {
+      delete self.map.entities[self.x][self.y][self.name];
+      for(name in self.map.entities[self.x][self.y]) { self.map.entities[self.x][self.y][name].on_moved_from(self); }
+      self.x = x;
+      self.y = y;
+      for(name in self.map.entities[self.x][self.y]) { self.map.entities[self.x][self.y][name].on_moved_to(self); }
+      self.map.entities[self.x][self.y][self.name] = self;
     }
 
     // ----- Initialize
-    if(args.ticks == true) { this.game_tick_subscription_number = game.on_tick.subscribe(this,'on_tick','',false); }
-    game.entities[this.name] = this;
-    this.setPosition(this.x,this.y);
-    this.current_image = this.sprites[0];
+    if(args.ticks == true) { self.game_tick_subscription_number = game.on_tick.subscribe(self.ontick,false,false); }
+    game.entities[self.name] = self;
+    self.setPosition(self.x,self.y);
+    self.current_image = self.sprites[0];
   }
 // ----- }
