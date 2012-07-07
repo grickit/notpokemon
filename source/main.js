48a25ef92b5eabc25bf1b302c81180086c89c389
Derek Hoagland <thegrickit@gmail.com>
2012-07-07 11:09:49 -0400

    Remove duplicate tile.

--- source/main.js
+++ source/main.js
@@ -44,7 +44,6 @@ function start() {
   baseTileSet.add({code: 'dg-t',sprite: new Sprite({imageURL: 'tiles/dirttograss',x: 17,y: 34})});
   baseTileSet.add({code: 'dg-tr',sprite: new Sprite({imageURL: 'tiles/dirttograss',x: 34,y: 34})});
   baseTileSet.add({code: 'dg-ml',sprite: new Sprite({imageURL: 'tiles/dirttograss',x: 0,y: 51})});
-  baseTileSet.add({code: 'dg-m',sprite: new Sprite({imageURL: 'tiles/dirttograss',x: 17,y: 51})});
   baseTileSet.add({code: 'dg-mr',sprite: new Sprite({imageURL: 'tiles/dirttograss',x: 34,y: 51})});
   baseTileSet.add({code: 'dg-bl',sprite: new Sprite({imageURL: 'tiles/dirttograss',x: 0,y: 68})});
   baseTileSet.add({code: 'dg-b',sprite: new Sprite({imageURL: 'tiles/dirttograss',x: 17,y: 68})});
